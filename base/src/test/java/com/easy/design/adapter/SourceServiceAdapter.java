775a8806a1d6c455448f38e03c2ae7155ec847f7
DESKTOP-BKW0JD2\cai <caijunjunit@163.com>
2017-02-04 14:42:20 +0800

    注释补充

--- base/src/test/java/com/easy/design/adapter/SourceServiceAdapter.java
+++ base/src/test/java/com/easy/design/adapter/SourceServiceAdapter.java
@@ -1,5 +1,14 @@
 package com.easy.design.adapter;
 
+/**
+ * 
+ * @Title: SourceServiceAdapter.java 
+ * @Package com.easy.design.adapter 
+ * @Description: 接口适配旧接口
+ * @author caiJunJun
+ * @date 2017年2月4日
+ *
+ */
 public interface SourceServiceAdapter extends SourceService {
 	public void send();
 }
--- base/src/test/java/com/easy/design/adapter/SourceServiceAdapterImpl.java
+++ base/src/test/java/com/easy/design/adapter/SourceServiceAdapterImpl.java
@@ -1,5 +1,13 @@
 package com.easy.design.adapter;
-
+/**
+ * 
+ * @Title: SourceServiceAdapterImpl.java 
+ * @Package com.easy.design.adapter 
+ * @Description: 实现适配旧实现
+ * @author caiJunJun
+ * @date 2017年2月4日
+ *
+ */
 public class SourceServiceAdapterImpl extends SourceServiceImpl implements SourceServiceAdapter {
 
 	@Override
